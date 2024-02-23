3cda1d6ffe6f7fe458c9acbd973c777fe46dd7bc
Ray Li <ray.li.verified@gmail.com>
2024-02-23 12:33:02 -0600

    Flutter v3.19 Update

---
+++ android/app/src/main/kotlin/com/pub_dev/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.pub_dev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/pubdev/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.pubdev
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
